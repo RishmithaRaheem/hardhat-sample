a5d259ed4fa36f6dec8a6f3424b8a55e9ed244eb
Rish's Mac <apple@Apples-MacBook-Pro.local>
2023-07-04 01:38:00 +0530

    createNewEvent function created

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 contract Web3RSVP {
-    struct CreateEvent {
+    struct CreateEvent {  // state varaible and hence preallocated in storage 
         bytes32 eventId;
         string eventDataCID;
         address eventOwner;
@@ -16,4 +16,36 @@ contract Web3RSVP {
 
    mapping(bytes32 => CreateEvent) public idToEvent; //mapping is like hastable. bytes32 is datatype of key and CreateEvent is datatype of value, view is publis, idToEvent is the name of dictionary
    
+   function createNewEvent(uint256 eventTimestamp,  
+    uint256 deposit, 
+    uint256 maxCapacity, 
+    string calldata eventDataCID) external {
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
+        require(idToEvent[eventId].eventTimestamp == 0, "ALREADY REGISTERED");
+
+        address[] memory confirmedRSVPs; // array of addresses(Etherium accounts) stored in memory of call stack of function rather than storage of smart contract. Lives only inside the funtion and goes away function returns something, meaning it leaves the function stack
+        address[] memory claimedRSVPs; //local in memory variable and doesnt change the state of contract in blockchain 
+
+        // this creates a new CreateEvent struct and adds it to the idToEvent mapping
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+
+    } 
 }
